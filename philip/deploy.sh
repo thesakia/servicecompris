55c294c65b98d8fd94ad0223207bbd8fdfded5fc
FT <fenauxft@gmail.com>
2026-05-22 16:01:32 +0200

    Move Philip and Doo copies to ftfenaux hosting

--- philip/deploy.sh
+++ philip/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SERVER="${1:-monvps}"
-REMOTE="/var/www/ftfenaux/servicecompris/doo"
+REMOTE="/var/www/ftfenaux/servicecompris/philip"
 LOCAL="$(dirname "$0")"
 
 rsync -avz --delete \
   --exclude='.git' --exclude='.claude' --exclude='deploy.sh' \
   "$LOCAL/" "$SERVER:$REMOTE/"
 
-echo "Done -> https://ftfenaux.com/servicecompris/doo/"
+echo "Done -> https://ftfenaux.com/servicecompris/philip/"
